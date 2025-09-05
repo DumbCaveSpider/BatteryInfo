3b0fc617f8c6ca47cd2db7d45887ee802b0f7cc5
DumbCaveSpider <git@arcticwoof.xyz>
2025-09-05 16:25:34 +0800

    ios?

--- src/devices/Ios.mm
+++ src/devices/Ios.mm
@@ -1,6 +1,7 @@
 #if defined(__APPLE__) && (TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR)
 #import <UIKit/UIKit.h>
 #include <BatteryInfo.hpp>
+#include <cmath>
 
 using namespace arcticwoof;
 
@@ -8,9 +9,10 @@ using namespace arcticwoof;
         @autoreleasepool {
             UIDevice* d = [UIDevice currentDevice];
             d.batteryMonitoringEnabled = YES;
+            [NSThread sleepForTimeInterval:0.01];            
             float level = d.batteryLevel; // -1.0 if unknown
             if (level < 0.0f) return -1;
-            return static_cast<int>(level * 100.0f);
+            return static_cast<int>(round(level * 100.0f));
         }
     }
 
