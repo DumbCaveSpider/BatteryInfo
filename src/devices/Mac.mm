9b3a217685e530b863bf901d0129e392ecf14dbc
DumbCaveSpider <markiel_giles@outlook.com>
2025-09-05 10:35:52 +0800

    now mac time

---
+++ src/devices/Mac.mm
@@ -0,0 +1,113 @@
+#include "../BatteryInfo.hpp"
+#if defined(__APPLE__) && !(TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR)
+#import <Foundation/Foundation.h>
+#import <IOKit/ps/IOPowerSources.h>
+#import <IOKit/ps/IOPSKeys.h>
+
+float BatteryInfo::getBatteryLevel() {
+    @autoreleasepool {
+        CFTypeRef powerSourceInfo = IOPSCopyPowerSourcesInfo();
+        if (!powerSourceInfo) return -1.0f;
+        
+        CFArrayRef powerSources = IOPSCopyPowerSourcesList(powerSourceInfo);
+        if (!powerSources) {
+            CFRelease(powerSourceInfo);
+            return -1.0f;
+        }
+        
+        CFIndex count = CFArrayGetCount(powerSources);
+        if (count == 0) {
+            CFRelease(powerSources);
+            CFRelease(powerSourceInfo);
+            return -1.0f;
+        }
+        
+        CFDictionaryRef powerSource = NULL;
+        for (CFIndex i = 0; i < count; i++) {
+            powerSource = (CFDictionaryRef)CFArrayGetValueAtIndex(powerSources, i);
+            CFStringRef powerSourceState = (CFStringRef)CFDictionaryGetValue(powerSource, CFSTR(kIOPSTypeKey));
+            
+            if (powerSourceState && CFEqual(powerSourceState, CFSTR(kIOPSInternalBatteryType))) {
+                break;
+            }
+            powerSource = NULL;
+        }
+        
+        if (!powerSource) {
+            CFRelease(powerSources);
+            CFRelease(powerSourceInfo);
+            return -1.0f;
+        }
+        
+        CFNumberRef currentCapacity = (CFNumberRef)CFDictionaryGetValue(powerSource, CFSTR(kIOPSCurrentCapacityKey));
+        CFNumberRef maxCapacity = (CFNumberRef)CFDictionaryGetValue(powerSource, CFSTR(kIOPSMaxCapacityKey));
+        
+        int currentValue = 0;
+        int maxValue = 0;
+        
+        if (currentCapacity && maxCapacity &&
+            CFNumberGetValue(currentCapacity, kCFNumberIntType, &currentValue) &&
+            CFNumberGetValue(maxCapacity, kCFNumberIntType, &maxValue) &&
+            maxValue > 0) {
+            
+            float percentage = (float)currentValue / (float)maxValue * 100.0f;
+            
+            CFRelease(powerSources);
+            CFRelease(powerSourceInfo);
+            
+            return percentage;
+        }
+        
+        CFRelease(powerSources);
+        CFRelease(powerSourceInfo);
+    }
+    
+    return -1.0f;
+}
+
+bool BatteryInfo::isCharging() {
+    @autoreleasepool {
+        CFTypeRef powerSourceInfo = IOPSCopyPowerSourcesInfo();
+        if (!powerSourceInfo) return false;
+        
+        CFArrayRef powerSources = IOPSCopyPowerSourcesList(powerSourceInfo);
+        if (!powerSources) {
+            CFRelease(powerSourceInfo);
+            return false;
+        }
+        
+        CFIndex count = CFArrayGetCount(powerSources);
+        if (count == 0) {
+            CFRelease(powerSources);
+            CFRelease(powerSourceInfo);
+            return false;
+        }
+        
+        CFDictionaryRef powerSource = NULL;
+        for (CFIndex i = 0; i < count; i++) {
+            powerSource = (CFDictionaryRef)CFArrayGetValueAtIndex(powerSources, i);
+            CFStringRef powerSourceState = (CFStringRef)CFDictionaryGetValue(powerSource, CFSTR(kIOPSTypeKey));
+            
+            if (powerSourceState && CFEqual(powerSourceState, CFSTR(kIOPSInternalBatteryType))) {
+                break;
+            }
+            powerSource = NULL;
+        }
+        
+        if (!powerSource) {
+            CFRelease(powerSources);
+            CFRelease(powerSourceInfo);
+            return false;
+        }
+        
+        CFStringRef charging = (CFStringRef)CFDictionaryGetValue(powerSource, CFSTR(kIOPSPowerSourceStateKey));
+        
+        bool isCharging = charging && CFEqual(charging, CFSTR(kIOPSACPowerValue));
+        
+        CFRelease(powerSources);
+        CFRelease(powerSourceInfo);
+        
+        return isCharging;
+    }
+}
+#endif
