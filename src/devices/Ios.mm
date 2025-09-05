cf7c93c96e566ab009eb766582d28b50ee3daa57
DumbCaveSpider <git@arcticwoof.xyz>
2025-09-05 16:49:36 +0800

    ios not being correct sad

--- src/devices/Ios.mm
+++ src/devices/Ios.mm
@@ -8,8 +8,11 @@ using namespace arcticwoof;
     int BatteryInfo::getBatteryLevel() {
         @autoreleasepool {
             UIDevice* d = [UIDevice currentDevice];
-            d.batteryMonitoringEnabled = YES;
-            [NSThread sleepForTimeInterval:0.01];            
+            if (!d.batteryMonitoringEnabled) {
+                d.batteryMonitoringEnabled = YES;
+                [NSThread sleepForTimeInterval:0.05];
+            }
+            
             float level = d.batteryLevel; // -1.0 if unknown
             if (level < 0.0f) return -1;
             return static_cast<int>(round(level * 100.0f));
@@ -19,7 +22,11 @@ using namespace arcticwoof;
     bool BatteryInfo::isCharging() {
         @autoreleasepool {
             UIDevice* d = [UIDevice currentDevice];
-            d.batteryMonitoringEnabled = YES;
+            if (!d.batteryMonitoringEnabled) {
+                d.batteryMonitoringEnabled = YES;
+                [NSThread sleepForTimeInterval:0.05];
+            }
+            
             UIDeviceBatteryState s = d.batteryState;
             return (s == UIDeviceBatteryStateCharging || s == UIDeviceBatteryStateFull);
         }
