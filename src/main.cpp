9248de458c500b3c038b4036a3b16ac0679ac7a0
DumbCaveSpider <markiel_giles@outlook.com>
2025-09-05 10:14:44 +0800

    visual label test

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,37 @@
 
 using namespace geode::prelude;
 
-class $modify(MyPlayLayer, PlayLayer) {
+class $modify(MyPlayLayer, PlayLayer)
+{
 
-    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+    bool init(GJGameLevel *level, bool useReplay, bool dontCreateObjects)
+    {
 
-    if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects))
+            return false;
 
         float batteryLevel = BatteryInfo::getBatteryLevel();
         bool charging = BatteryInfo::isCharging();
 
-        if (batteryLevel >= 0.0f) {
-            log::debug("Battery: %.0f%%  charging=%d", batteryLevel, charging);
-        } else {
+        if (batteryLevel >= 0.0f)
+        {
+            log::debug("Battery: {}%  charging={}", batteryLevel, charging);
+            auto label = CCLabelBMFont::create(
+                (std::to_string(static_cast<int>(batteryLevel)) + "%").c_str(),
+                "bigFont.fnt");
+            label->setPosition({winSize.width, winSize.height});
+            this->addChild(label);
+        }
+        else
+        {
             log::debug("Battery: unknown");
+            auto label = CCLabelBMFont::create(
+                "N/A",
+                "bigFont.fnt");
+            label->setPosition({winSize.width, winSize.height});
+            this->addChild(label);
         }
 
         return true;
